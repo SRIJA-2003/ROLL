ba1557024e881fd4e588c259ba06d858ca0fb7b0
SRIJA-2003 <113287988+SRIJA-2003@users.noreply.github.com>
2022-09-22 19:53:30 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,11 @@
+
+import pickle
+import streamlit as st
+pickle_in=open(Roll.pkl','rb')
+model=pickle.load(pickle_in)
+a=st.number_input('Enter NAME')
+result=''
+
+if st.predict('PREDICT'):
+   result=st.predict([[a]])
+   st.success(result)
